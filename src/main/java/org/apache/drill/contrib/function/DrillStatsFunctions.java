707a16a443191d4a066cfa16b95fc95e5f6b02bc
cgivre <cgivre@gmail.com>
2018-01-01 18:12:56 -0500

    Added Pearson's R

--- src/main/java/org/apache/drill/contrib/function/DrillStatsFunctions.java
+++ src/main/java/org/apache/drill/contrib/function/DrillStatsFunctions.java
@@ -23,6 +23,7 @@ import org.apache.drill.exec.expr.annotations.Output;
 import org.apache.drill.exec.expr.annotations.Param;
 import org.apache.drill.exec.expr.annotations.Workspace;
 import org.apache.drill.exec.expr.holders.Float8Holder;
+import org.apache.drill.exec.expr.holders.NullableFloat8Holder;
 import org.apache.drill.exec.expr.holders.IntHolder;
 import org.apache.drill.exec.expr.holders.ObjectHolder;
 
@@ -32,136 +33,224 @@ import java.util.Comparator;
 
 public class DrillStatsFunctions {
 
-    @FunctionTemplate(name = "median",
-        scope = FunctionTemplate.FunctionScope.POINT_AGGREGATE,
-        nulls = FunctionTemplate.NullHandling.INTERNAL)
+  @FunctionTemplate(name = "median",
+      scope = FunctionTemplate.FunctionScope.POINT_AGGREGATE,
+      nulls = FunctionTemplate.NullHandling.INTERNAL)
 
-    public static class MedianFunction implements DrillAggFunc {
-        @Param
-        Float8Holder in;
+  public static class MedianFunction implements DrillAggFunc {
+    @Param
+    Float8Holder in;
 
-        @Output
-        Float8Holder out;
+    @Output
+    Float8Holder out;
 
-        @Workspace
-        Float8Holder median;
+    @Workspace
+    Float8Holder median;
 
-        @Workspace
-        ObjectHolder minStackHolder;
+    @Workspace
+    ObjectHolder minStackHolder;
 
-        @Workspace
-        ObjectHolder maxStackHolder;
+    @Workspace
+    ObjectHolder maxStackHolder;
 
-        @Workspace
-        IntHolder counter;
+    @Workspace
+    IntHolder counter;
 
-        @Inject
-        DrillBuf buffer;
+    @Inject
+    DrillBuf buffer;
 
-        @Inject
-        DrillBuf buffer1;
+    @Inject
+    DrillBuf buffer1;
 
-        @Override
-        public void setup() {
-            median.value = 0.0;
-            counter.value = 0;
+    @Override
+    public void setup() {
+      median.value = 0.0;
+      counter.value = 0;
 
-            minStackHolder = new ObjectHolder();
-            maxStackHolder = new ObjectHolder();
+      minStackHolder = new ObjectHolder();
+      maxStackHolder = new ObjectHolder();
 
-            java.util.PriorityQueue<Double> minHeap =  minHeap = new java.util.PriorityQueue<Double>();
-            java.util.PriorityQueue<Double> maxHeap = new java.util.PriorityQueue<Double>(11, java.util.Collections.reverseOrder());
+      java.util.PriorityQueue<Double> minHeap = minHeap = new java.util.PriorityQueue<Double>();
+      java.util.PriorityQueue<Double> maxHeap = new java.util.PriorityQueue<Double>(11, java.util.Collections.reverseOrder());
 
-            minStackHolder.obj = minHeap;
-            maxStackHolder.obj = maxHeap;
-
-        }
-
-        @Override
-        public void add() {
-            java.util.PriorityQueue<Double> minHeap = (java.util.PriorityQueue<Double>)minStackHolder.obj;
-            java.util.PriorityQueue<Double> maxHeap = (java.util.PriorityQueue<Double>)maxStackHolder.obj;
-
-            double x = in.value;
-
-            //Case one first item.
-            if (counter.value == 0) {
-                median.value = x;
-                minHeap.add( new Double(x));
-            } else if (minHeap.size() == 1 && maxHeap.size() == 0) {
-                //Case for second iteration
-                if (x > ((Double)minHeap.peek()).doubleValue()) {
-                    maxHeap.add( new Double(x));
-                    median.value = (((Double)minHeap.peek()).doubleValue() + x ) / 2.0;
-                } else {
-                    Double y = (Double)minHeap.poll();
-                    maxHeap.add(y);
-                    minHeap.add( new Double( x ));
-                    median.value = (x + y.doubleValue()) / 2.0;
-                }
-            } else {
-                if (x < median.value) {
-                    minHeap.add( new Double( x ) );
-                } else if (x > median.value) {
-                    maxHeap.add( new Double(x));
-                } else {
-                    if (minHeap.size() > maxHeap.size()) {
-                        maxHeap.add( new Double( x ) );
-                    } else {
-                        minHeap.add( new Double( x) );
-                    }
-                }
-
-                int difference = maxHeap.size() - minHeap.size();
-                if (difference > 1) {
-                    Double t = (Double) maxHeap.poll();
-                    minHeap.add(  t  );
-                } else {
-                    Double t = (Double)minHeap.poll();
-                    maxHeap.add( t );
-                }
-
-                if (minHeap.size() == maxHeap.size()) {
-                    median.value = ( ((Double)minHeap.peek()).doubleValue() + ((Double)maxHeap.peek()).doubleValue() ) / 2.0;
-                } else {
-                    if (maxHeap.size() > minHeap.size()) {
-                        median.value = ((Double)maxHeap.peek()).doubleValue();
-                    } else {
-                        median.value = ((Double)minHeap.peek()).doubleValue();
-                    }
-                }
-            }
-
-            counter.value = counter.value + 1;
-        }
-
-        @Override
-        public void output() {
-            out.value = median.value;
-        }
-
-        @Override
-        public void reset() {
-            median.value = 0.0;
-            counter.value = 0;
-
-
-            minStackHolder = new ObjectHolder();
-            maxStackHolder = new ObjectHolder();
-
-            java.util.PriorityQueue<Double> minHeap =  minHeap = new java.util.PriorityQueue<Double>();
-            java.util.PriorityQueue<Double> maxHeap = new java.util.PriorityQueue<Double>(11, java.util.Collections.reverseOrder());
-
-            minStackHolder.obj = minHeap;
-            maxStackHolder.obj = maxHeap;
-
-        }
-        
-        public class MyComparator implements Comparator<Double> {
-            public int compare(Double x, Double y) {
-                return (int) (y.doubleValue() - x.doubleValue());
-            }
-        }
+      minStackHolder.obj = minHeap;
+      maxStackHolder.obj = maxHeap;
 
     }
+
+    @Override
+    public void add() {
+      java.util.PriorityQueue<Double> minHeap = (java.util.PriorityQueue<Double>) minStackHolder.obj;
+      java.util.PriorityQueue<Double> maxHeap = (java.util.PriorityQueue<Double>) maxStackHolder.obj;
+
+      double x = in.value;
+
+      //Case one first item.
+      if (counter.value == 0) {
+        median.value = x;
+        minHeap.add(new Double(x));
+      } else if (minHeap.size() == 1 && maxHeap.size() == 0) {
+        //Case for second iteration
+        if (x > ((Double) minHeap.peek()).doubleValue()) {
+          maxHeap.add(new Double(x));
+          median.value = (((Double) minHeap.peek()).doubleValue() + x) / 2.0;
+        } else {
+          Double y = (Double) minHeap.poll();
+          maxHeap.add(y);
+          minHeap.add(new Double(x));
+          median.value = (x + y.doubleValue()) / 2.0;
+        }
+      } else {
+        if (x < median.value) {
+          minHeap.add(new Double(x));
+        } else if (x > median.value) {
+          maxHeap.add(new Double(x));
+        } else {
+          if (minHeap.size() > maxHeap.size()) {
+            maxHeap.add(new Double(x));
+          } else {
+            minHeap.add(new Double(x));
+          }
+        }
+
+        int difference = maxHeap.size() - minHeap.size();
+        if (difference > 1) {
+          Double t = (Double) maxHeap.poll();
+          minHeap.add(t);
+        } else {
+          Double t = (Double) minHeap.poll();
+          maxHeap.add(t);
+        }
+
+        if (minHeap.size() == maxHeap.size()) {
+          median.value = (((Double) minHeap.peek()).doubleValue() + ((Double) maxHeap.peek()).doubleValue()) / 2.0;
+        } else {
+          if (maxHeap.size() > minHeap.size()) {
+            median.value = ((Double) maxHeap.peek()).doubleValue();
+          } else {
+            median.value = ((Double) minHeap.peek()).doubleValue();
+          }
+        }
+      }
+
+      counter.value = counter.value + 1;
+    }
+
+    @Override
+    public void output() {
+      out.value = median.value;
+    }
+
+    @Override
+    public void reset() {
+      median.value = 0.0;
+      counter.value = 0;
+
+
+      minStackHolder = new ObjectHolder();
+      maxStackHolder = new ObjectHolder();
+
+      java.util.PriorityQueue<Double> minHeap = minHeap = new java.util.PriorityQueue<Double>();
+      java.util.PriorityQueue<Double> maxHeap = new java.util.PriorityQueue<Double>(11, java.util.Collections.reverseOrder());
+
+      minStackHolder.obj = minHeap;
+      maxStackHolder.obj = maxHeap;
+
+    }
+
+    public class MyComparator implements Comparator<Double> {
+      public int compare(Double x, Double y) {
+        return (int) (y.doubleValue() - x.doubleValue());
+      }
+    }
+
+  }
+
+  @FunctionTemplate(
+      name = "pearson_correlation",
+      scope = FunctionTemplate.FunctionScope.POINT_AGGREGATE,
+      nulls = FunctionTemplate.NullHandling.INTERNAL
+  )
+
+  public class PCorrelation implements DrillAggFunc {
+
+    @Param
+    NullableFloat8Holder xInput;
+
+    @Param
+    NullableFloat8Holder yInput;
+
+    @Workspace
+    IntHolder numValues;
+
+    @Workspace
+    Float8Holder xSum;
+
+    @Workspace
+    Float8Holder ySum;
+
+    @Workspace
+    Float8Holder xSqSum;
+
+    @Workspace
+    Float8Holder ySqSum;
+
+    @Workspace
+    Float8Holder xySum;
+
+    @Output
+    Float8Holder output;
+
+    public void setup() {
+      // Initialize values
+      numValues.value = 0;
+      xSum.value = 0;
+      ySum.value = 0;
+      xSqSum.value = 0;
+      ySqSum.value = 0;
+      xySum.value = 0;
+    }
+
+    public void reset() {
+      // Initialize values
+      numValues.value = 0;
+      xSum.value = 0;
+      ySum.value = 0;
+      xSqSum.value = 0;
+      ySqSum.value = 0;
+      xySum.value = 0;
+    }
+
+    public void add() {
+
+      // Only proceed if both floats aren't nulls
+      if ((xInput.isSet == 1) || (yInput.isSet == 1)) {
+        numValues.value++;
+
+        xSum.value += xInput.value;
+        ySum.value += yInput.value;
+
+        xSqSum.value += xInput.value * xInput.value;
+        ySqSum.value += yInput.value * yInput.value;
+
+        xySum.value += xInput.value * yInput.value;
+      }
+    }
+
+    public void output() {
+
+      float n = numValues.value;
+
+      double x = xSum.value;
+      double y = ySum.value;
+
+      double x2 = xSqSum.value;
+      double y2 = ySqSum.value;
+
+      double xy = xySum.value;
+
+      output.value = (n * xy - x * y) / (Math.sqrt(n * x2 - x * x) * Math.sqrt(n * y2 - y * y));
+    }
+  }
+
 }
